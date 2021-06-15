b438a26c6c5f4178a5d9efb44e0931bfff88d1cc
nabepa <nabepadev@gmail.com>
2021-06-15 19:30:35 +0900

    add cors, morgan

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
 import express from 'express';
-// import cors from 'cors';
-// import morgan from 'morgan';
+import cors from 'cors';
+import morgan from 'morgan';
 import helmet from 'helmet';
 import 'express-async-errors';
-import tweetRouter from './router/tweet.js';
+import tweetsRoute from './router/tweet.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(helmet());
+app.use(cors());
+app.use(morgan('tiny'));
 
-app.use('/tweets', tweetRouter);
+app.use('/tweets', tweetsRoute);
 
 app.use((req, res, next) => {
   res.sendStatus(404);
@@ -18,7 +20,7 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
   console.error(err);
-  res.status(500).json({ message: 'Something went wrong' });
+  res.sendStatus(500);
 });
 
 app.listen(8080);
