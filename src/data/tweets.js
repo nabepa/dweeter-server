4ea435321db0227d6c672cacc4e1aaa0d4026bcc
nabepa <nabepadev@gmail.com>
2021-06-16 13:18:15 +0900

    make data repository

---
+++ src/data/tweets.js
@@ -0,0 +1,54 @@
+let tweets = [
+  {
+    id: '1',
+    text: 'Exciting Backend 🙈',
+    createdAt: new Date(),
+    name: 'Bob',
+    username: 'bob',
+    url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
+  },
+  {
+    id: '2',
+    text: 'Abrakadabra ☠️',
+    createdAt: new Date(),
+    name: 'Julia',
+    username: 'joy1990',
+    url: 'https://cdn.expcloud.co/life/uploads/2020/04/27135731/Fee-gentry-hed-shot-1.jpg',
+  },
+];
+
+export function getAll() {
+  return tweets;
+}
+
+export function getAllByUsername(username) {
+  return tweets.filter((tweet) => tweet.username === username);
+}
+
+export function getById(id) {
+  return tweets.find((tweet) => tweet.id === id);
+}
+
+export function create(text, name, username) {
+  const tweet = {
+    id: Date.now().toString(),
+    text,
+    createdAt: new Date(),
+    name,
+    username,
+  };
+  tweets = [tweet, ...tweets];
+  return tweet;
+}
+
+export function update(id, text) {
+  const tweet = tweets.find((tweet) => tweet.id === id);
+  if (tweet) {
+    tweet.text = text;
+  }
+  return tweet;
+}
+
+export function remove(id) {
+  tweets = tweets.filter((tweet) => tweet.id !== id);
+}
--- src/router/tweets.js
+++ src/router/tweets.js
@@ -1,39 +1,20 @@
 import express from 'express';
 import 'express-async-errors';
-
-// 나중에 DB로 교체할 것
-let tweets = [
-  {
-    id: '1',
-    text: 'Exciting Backend 🙈',
-    createdAt: new Date(),
-    name: 'Bob',
-    username: 'bob',
-    url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
-  },
-  {
-    id: '2',
-    text: 'Abrakadabra ☠️',
-    createdAt: new Date(),
-    name: 'Julia',
-    username: 'joy1990',
-    url: 'https://cdn.expcloud.co/life/uploads/2020/04/27135731/Fee-gentry-hed-shot-1.jpg',
-  },
-];
+import * as tweetRepository from '../data/tweets.js';
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
   const username = req.query.username;
   const data = username
-    ? tweets.filter((tweet) => tweet.username === username)
-    : tweets;
+    ? tweetRepository.getAllByUsername(username)
+    : tweetRepository.getAll();
   res.status(200).json(data);
 });
 
 router.get('/:id', (req, res, next) => {
   const id = req.params.id;
-  const tweet = tweets.find((tweet) => tweet.id === id);
+  const tweet = tweetRepository.getById(id);
   if (tweet) {
     res.status(200).json(tweet);
   } else {
@@ -43,23 +24,15 @@ router.get('/:id', (req, res, next) => {
 
 router.post('/', (req, res, next) => {
   const { text, name, username } = req.body;
-  const tweet = {
-    id: Date.now().toString(),
-    text,
-    createdAt: new Date(),
-    name,
-    username,
-  };
-  tweets = [tweet, ...tweets];
+  const tweet = tweetRepository.create(text, name, username);
   res.status(201).json(tweet);
 });
 
 router.put('/:id', (req, res, next) => {
   const id = req.params.id;
   const text = req.body.text;
-  const tweet = tweets.find((tweet) => tweet.id === id);
+  const tweet = tweetRepository.update(id, text);
   if (tweet) {
-    tweet.text = text;
     res.status(200).json(tweet);
   } else {
     res.status(404).json({ message: `Tweed id(${id}) not found` });
@@ -68,7 +41,7 @@ router.put('/:id', (req, res, next) => {
 
 router.delete('/:id', (req, res, next) => {
   const id = req.params.id;
-  tweets = tweets.filter((tweet) => tweet.id !== id);
+  tweetRepository.remove(id);
   res.sendStatus(204);
 });
 
