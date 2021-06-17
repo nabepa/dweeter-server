8a977d163428eda26a15fbebed459e84949b8d80
nabepa <nabepadev@gmail.com>
2021-06-17 13:14:19 +0900

    validation

--- src/router/tweets.js
+++ src/router/tweets.js
@@ -1,17 +1,30 @@
 import express from 'express';
 import 'express-async-errors';
+import { body } from 'express-validator';
 import * as tweetController from '../controller/tweet.js';
+import { validate } from '../middleware/validator.js';
 
 const router = express.Router();
+
+// validation
+const validateTweet = [
+  body('text')
+    .trim()
+    .isLength({ min: 3, max: 140 })
+    .withMessage(
+      'text should be at least 3 characters and no more than 140 characters'
+    ),
+  validate,
+];
 
 // 주의) 함수를 호출하면 값을 연결해주는 것이 됨, 함수를 연결해주어야 함
 router.get('/', tweetController.getTweets);
 
 router.get('/:id', tweetController.getTweet);
 
-router.post('/', tweetController.createTweet);
+router.post('/', validateTweet, tweetController.createTweet);
 
-router.put('/:id', tweetController.updateTweet);
+router.put('/:id', validateTweet, tweetController.updateTweet);
 
 router.delete('/:id', tweetController.deleteTweet);
 
