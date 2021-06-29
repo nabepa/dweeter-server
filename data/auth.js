22798caa78d64b718457832171a092de63c03f68
nabepa <nabepadev@gmail.com>
2021-06-29 14:42:48 +0900

    use mysql for users information

--- data/auth.js
+++ data/auth.js
@@ -1,33 +1,23 @@
-// 12345: $2b$12$DGnH5XZHa45Kl..JdMyL2.hPigT0XHUyZds.NaABsDHesdPUhwmXm
-let users = [
-  {
-    id: '1',
-    username: 'bob',
-    password: '$2b$12$DGnH5XZHa45Kl..JdMyL2.hPigT0XHUyZds.NaABsDHesdPUhwmXm',
-    name: 'Bob',
-    email: 'bob@gmail.com',
-    url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-1.png',
-  },
-  {
-    id: '2',
-    username: 'ellie',
-    password: '$2b$12$DGnH5XZHa45Kl..JdMyL2.hPigT0XHUyZds.NaABsDHesdPUhwmXm',
-    name: 'Ellie',
-    email: 'ellie@gmail.com',
-    url: 'https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-4-300x300.png',
-  },
-];
+import { db } from '../db/database.js';
 
 export async function findByUsername(username) {
-  return users.find((user) => user.username === username);
+  return db
+    .execute('SELECT * FROM users WHERE username=?', [username]) //
+    .then((result) => result[0][0]);
 }
 
 export async function findById(id) {
-  return users.find((user) => user.id === id);
+  return db
+    .execute('SELECT * FROM users WHERE id=?', [id]) //
+    .then((result) => result[0][0]);
 }
 
 export async function createUser(user) {
-  const created = { ...user, id: Date.now().toString() };
-  users.push(created);
-  return created.id;
+  const { username, password, name, email, url } = user;
+  return db
+    .execute(
+      'INSERT INTO users (username, password, name, email, url) VALUES (?, ?, ?, ?, ?)',
+      [username, password, name, email, url]
+    )
+    .then((result) => result[0].insertId);
 }
