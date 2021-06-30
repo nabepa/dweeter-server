d6861910b65883dbbcd1b251fc4873cd48b49fd0
nabepa <nabepadev@gmail.com>
2021-06-30 12:47:16 +0900

    test

--- config.js
+++ config.js
@@ -10,18 +10,18 @@ function required(key, defalutValue = undefined) {
   return value;
 }
 export const config = {
+  db: {
+    host: required('DB_HOST'),
+    user: required('DB_USER'),
+    database: required('DB_DATABASE'),
+    password: required('DB_PASSWORD'),
+  },
   jwt: {
     secretKey: required('JWT_SECRET'),
     expiresInSec: parseInt(required('JWT_EXPIRES_SEC', 86400)),
   },
   bcrypt: {
     saltRounds: parseInt(required('BCRYPT_SALT_ROUND', 12)),
-  },
-  db: {
-    host: required('DB_HOST'),
-    user: required('DB_USER'),
-    database: required('DB_DATABASE'),
-    password: required('DB_PASSWORD'),
   },
   port: parseInt(required('PORT', 8080)),
   cors: {
